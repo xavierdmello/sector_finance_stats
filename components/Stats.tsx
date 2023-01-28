6344ff79c3af161d7d5ed36e9dde2898626d1ac1
Xavier DMello <xaviermdmello@gmail.com>
2023-01-27 22:09:49 -0500

    added a ton of features

---
+++ components/Stats.tsx
@@ -0,0 +1,122 @@
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+
+import { useEffect, useState } from "react";
+import { useAccount, useContractRead } from "wagmi";
+import styles from "../styles/Stats.module.css";
+import { BigNumber, BytesLike, ethers } from "ethers";
+import Vault from "../abi/Vault";
+const moonscanKey = "F86IKT3Z84MNJENUW4RJRMMZIP2CRKXIF2";
+const vault = "0xc24D43093b44b7A9657571DDB79FEdf014eaef7d";
+const decimals = 6;
+const depositTopic = "0xe1fffcc4923d04b559f4d29a8bfc6cda04eb5b0d3c460751c2402c5c5cc9109c";
+const withdrawTopic = "0x884edad9ce6fa2440d8a54cc123490eb96d2768479d49ff9c7366125a9424364";
+
+function Stats() {
+  const [netDeposits, setNetDeposits] = useState("???");
+  const [pl, setPl] = useState("???");
+  const [balance, setBalance] = useState("???");
+  let { address } = useAccount();
+  if (!address) {
+    address = "0x0000000000000000000000000000000000000000";
+  }
+  let {
+    data: rawBalance,
+    isError,
+    isLoading,
+  } = useContractRead({
+    address: vault,
+    abi: Vault,
+    functionName: "balanceOfUnderlying",
+    args: [address],
+  });
+
+  function bnFormat(bn: BigNumber) {
+    const bnString = bn.toString();
+    const bnFormatted =
+      bnString === "0"
+        ? "0"
+        : bnString.substring(0, bnString.length - decimals) + "." + bnString.substring(bnString.length - decimals, bnString.length - decimals + 2);
+    return bnFormatted;
+  }
+
+  useEffect(() => {
+    async function load() {
+      if (!rawBalance) {
+        rawBalance = BigNumber.from(0);
+      }
+      setBalance(bnFormat(rawBalance));
+      const encodedAddress = ethers.utils.defaultAbiCoder.encode(["address"], [address]);
+
+      // Fetch events
+      const depositDataPromise = fetch(`https://api-moonriver.moonscan.io/api?module=logs&action=getLogs
+&fromBlock=0
+&toBlock=latest
+&address=${vault}
+&topic0=${depositTopic}
+&topic0_1_opr=and
+&topic1=${encodedAddress}
+&apikey=${moonscanKey}`);
+      const withdrawDataPromise = fetch(`https://api-moonriver.moonscan.io/api?module=logs&action=getLogs
+&fromBlock=0
+&toBlock=latest
+&address=${vault}
+&topic0=${withdrawTopic}
+&topic0_1_opr=and
+&topic1=${encodedAddress}
+&apikey=${moonscanKey}`);
+
+      // Parse events
+      const [depositData, withdrawData] = await Promise.all([depositDataPromise, withdrawDataPromise]);
+      let [rawDeposits, rawWithdrawals] = await Promise.all([depositData.json(), withdrawData.json()]);
+      rawDeposits = rawDeposits.result;
+      rawWithdrawals = rawWithdrawals.result;
+
+      // Tally deposits, withdrawals, and P/L
+      let depositTally = BigNumber.from(0);
+      let withdrawalTally = BigNumber.from(0);
+      let plCount = BigNumber.from(0);
+      rawDeposits.forEach((rawDeposit: any) => {
+        const deposit: BigNumber = ethers.utils.defaultAbiCoder.decode(["uint"], rawDeposit.data)[0];
+        depositTally = depositTally.add(deposit);
+      });
+      rawWithdrawals.forEach((rawWithdrawal: any) => {
+        const withdrawal: BigNumber = ethers.utils.defaultAbiCoder.decode(["uint"], rawWithdrawal.data)[0];
+        withdrawalTally = withdrawalTally.add(withdrawal);
+      });
+      const netDepositsCount = depositTally.sub(withdrawalTally);
+
+      if (balance) {
+        plCount = rawBalance.sub(netDepositsCount);
+      }
+
+      setNetDeposits(bnFormat(netDepositsCount));
+      setPl(bnFormat(plCount));
+    }
+    if (address !== "0x0000000000000000000000000000000000000000") {
+      load();
+    }
+  }, [address]);
+
+  return (
+    <div className={styles.container}>
+      <h1>Current Balance: {balance} USDC</h1>
+      <div className={styles.row}>
+        <h1>Net Deposits: {netDeposits} USDC</h1>
+        <p className={styles.formula}>(Total Deposists - Total Withdrawals)</p>
+      </div>
+
+      <h1>P/L: {pl} USDC</h1>
+      <center>
+        <a href="https://github.com/xavierdmello/sector_finance_stats">Github</a>
+        <p>
+          Data provided by <a href="https://moonriver.moonscan.io/">Moonriver Moonscan</a>
+        </p>
+        <p>
+          &copy; 2023 Xavier D'Mello <a href="https://twitter.com/nova_holo">@nova_holo</a>
+        </p>
+      </center>
+    </div>
+  );
+}
+
+export default Stats;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,53 +1,15 @@
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import type { NextPage } from "next";
-import Head from "next/head";
-import { useEffect, useState } from "react";
-import { useAccount } from "wagmi";
-import styles from "../styles/Home.module.css";
-import { BigNumber, BytesLike, ethers } from "ethers";
-
-const moonscanKey = "F86IKT3Z84MNJENUW4RJRMMZIP2CRKXIF2";
-const vault = "0xc24D43093b44b7A9657571DDB79FEdf014eaef7d";
-const decimals = 6;
+import Stats from "../components/Stats";
+import styles from "../styles/Home.module.css"
 
 const Home: NextPage = () => {
-  const [deposits, setDeposits] = useState("");
-
-  const { address } = useAccount();
-
-  useEffect(() => {
-    async function load() {
-      const encodedAddress = ethers.utils.defaultAbiCoder.encode(["address"], [address]);
-      const data = await fetch(`https://api-moonriver.moonscan.io/api?module=logs&action=getLogs
-&fromBlock=0
-&toBlock=latest
-&address=${vault}
-&topic0=0xe1fffcc4923d04b559f4d29a8bfc6cda04eb5b0d3c460751c2402c5c5cc9109c
-&topic0_1_opr=and
-&topic1=${encodedAddress}
-&apikey=${moonscanKey}`);
-
-      const rawDeposits = (await data.json()).result;
-      let depositTally = BigNumber.from(0);
-      rawDeposits.forEach((rawDeposit: any) => {
-        const deposit: BigNumber = ethers.utils.defaultAbiCoder.decode(["uint"], rawDeposit.data)[0];
-        depositTally = depositTally.add(deposit);
-      });
-      const totalDeposits = depositTally.toString();
-      const totalDepositsFormatted =
-        totalDeposits === "0"
-          ? "0"
-          : totalDeposits.substring(0, totalDeposits.length - decimals) + "." + totalDeposits.substring(0, totalDeposits.length - decimals - 2);
-          
-      setDeposits(totalDepositsFormatted);
-    }
-    load();
-  }, [address]);
-
   return (
-    <div className="container">
+    <div className={styles.container}>
+      <a href="https://scion.finance/">https://scion.finance/</a>
+      <h1>Sector Finance Stats</h1>
       <ConnectButton />
-      <h1>Net Deposits: {deposits} USDC</h1>
+      <Stats />
     </div>
   );
 };
